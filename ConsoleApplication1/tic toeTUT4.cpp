24a2a5fa5c84e95e9ae1b2db2f4302e1072c8dce
teeteejobe <212512921@stu.ukzn.ac.za>
2016-03-16 02:54:08 +0200

    Change_player

--- ConsoleApplication1/tic toeTUT4.cpp
+++ ConsoleApplication1/tic toeTUT4.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 void design();
 void input();
+char Status();
+void Position_check();
 void Change_player();
 char matrx[3][3] = { '1', '2', '3','4', '5', '6','7', '8', '9'};
 char player = 'x';
@@ -14,6 +16,16 @@ int main()
 	{
 		input();
 		design();
+		if (Status() == 'x')
+		{
+			cout << "x wins!:"<<endl;
+			break;
+		}
+		else if (Status() == '0')
+		{
+			cout << "x wins!:"<<endl;
+			break;
+		}
 		Change_player();
 	}
 	system("pause");
@@ -22,7 +34,7 @@ int main()
 // Function to draw playing court
 void design()
 {
-
+	system("cls");
 	int i, j;
 	for (i = 0; i<3; i++)
 	{
@@ -34,6 +46,7 @@ void design()
 		cout << endl;
 	}
 }
+//placing position for each player 
 void input()
 {
 	int b;
@@ -41,40 +54,67 @@ void input()
 	cin >> b;
 
 	if (b == 1)
-	{
-		matrx[0][0] = player;
+	{  
+		if (matrx[0][0] = !'x' || '0') //check if position has been taken
+		{
+			matrx[0][0] = player;
+		}
 	}
 	else if (b == 2)
 	{
-		matrx[0][1] = player;
+		if (matrx[0][1] = !'x' || '0')//check if position has been taken
+		{
+			matrx[0][1] = player;
+		}
 	}
 	else if (b == 3)
 	{
-		matrx[0][2] = player;
+		if (matrx[0][2] = !'x' || '0')//check if position has been taken
+		{
+			matrx[0][2] = player;
+		}
 	}
 	else if (b == 4)
 	{
-		matrx[1][0] = player;
+		if (matrx[1][0] = !'x' || '0')//check if position has been taken
+		{
+			matrx[1][0] = player;
+		}
 	}
 	else if (b == 5)
 	{
-		matrx[1][1] = player;
+		if (matrx[1][1] = !'x' || '0')//check if position has been taken
+		{
+			matrx[1][1] = player;
+		}
 	}
 	else if (b == 6)
 	{
-		matrx[1][2] = player;
+		if (matrx[1][2] = !'x' || '0')//check if position has been taken
+		{
+			matrx[1][2] = player;
+		}
 	}
 	else if (b == 7)
 	{
-		matrx[2][0] = player;
+		if (matrx[2][0] = !'x' || '0')//check if position has been taken
+		{
+			matrx[2][0] = player;
+		}
 	}
 	else if (b == 8)
 	{
-		matrx[2][1] = player;
+		if (matrx[2][1] = !'x' || '0')//check if position has been taken
+		{
+			matrx[2][1] = player;
+		}
 	}
 	else if (b == 9)
 	{
-		matrx[2][2] = player;
+		if (matrx[2][2] = !'x' || '0')//check if position has been taken
+		{
+			matrx[2][2] = player;
+		}
 	}
 }
 void Change_player()
@@ -85,3 +125,62 @@ void Change_player()
 		player = 'x';
 
 }
+char Status()
+{
+	// checking if player one wins 
+	if (matrx[0][0] == 'x' && matrx[0][1] == 'x' && matrx[0][2] == 'x')
+		return 'x';
+	if (matrx[1][0] == 'x' && matrx[1][1] == 'x' && matrx[1][2] == 'x')
+		return 'x';
+	if (matrx[2][0] == 'x' && matrx[2][1] == 'x' && matrx[2][2] == 'x')
+		return 'x'; 
+	if (matrx[0][0] == 'x' && matrx[1][0] == 'x' && matrx[2][0] == 'x')
+		return 'x';
+	if (matrx[0][1] == 'x' && matrx[1][1] == 'x' && matrx[2][1] == 'x')
+		return 'x';
+	if (matrx[0][2] == 'x' && matrx[1][2] == 'x' && matrx[2][2] == 'x')
+		return 'x';
+	if (matrx[0][0] == 'x' && matrx[1][1] == 'x' && matrx[2][2] == 'x')
+		return 'x';
+	if (matrx[0][2] == 'x' && matrx[1][1] == 'x' && matrx[2][0] == 'x')
+		return 'x';
+	//checking if player two wins
+	if (matrx[0][0] == '0' && matrx[0][1] == '0' && matrx[0][2] == '0')
+		return '0';
+	if (matrx[1][0] == '0' && matrx[1][1] == '0' && matrx[1][2] == '0')
+		return '0';
+	if (matrx[2][0] == '0' && matrx[2][1] == '0' && matrx[2][2] == '0')
+		return '0';
+	if (matrx[0][0] == '0' && matrx[1][0] == '0' && matrx[2][0] == '0')
+		return '0';
+	if (matrx[0][1] == '0' && matrx[1][1] == '0' && matrx[2][1] == '0')
+		return '0';
+	if (matrx[0][2] == '0' && matrx[1][2] == '0' && matrx[2][2] == '0')
+		return '0';
+	if (matrx[0][0] == '0' && matrx[1][1] == '0' && matrx[2][2] == '0')
+		return '0';
+	if (matrx[0][2] == '0' && matrx[1][1] == '0' && matrx[2][0] == '0')
+		return '0';
+
+	return '/';
+
+}
+/*void Position_check()
+{
+	if (matrx[0][0] == 'x' || '0' || matrx[0][1] == 'x' || '0' || matrx[0][2] == 'x' || '0')
+		return 'x';
+	if (matrx[1][0] == 'x' || '0' || matrx[1][1] == 'x' || '0' || matrx[1][2] == 'x' || '0')
+		return 'x';
+	if (matrx[2][0] == 'x' || '0' || matrx[2][1] == 'x' || '0' || matrx[2][2] == 'x' || '0')
+		return 'x';
+	if (matrx[0][0] == 'x' || '0' || matrx[1][0] == 'x' || '0' || matrx[2][0] == 'x' || '0')
+		return 'x';
+	if (matrx[0][1] == 'x' || '0' || matrx[1][1] == 'x' || '0' || matrx[2][1] == 'x'|| '0')
+		return 'x';
+	if (matrx[0][2] == 'x' || '0' || matrx[1][2] == 'x' || '0' || matrx[2][2] == 'x' || '0')
+		return 'x';
+	if (matrx[0][0] == 'x' || '0' || matrx[1][1] == 'x' || '0' || matrx[2][2] == 'x' || '0')
+		return 'x';
+	if (matrx[0][2] == 'x' || '0' || matrx[1][1] == 'x' || '0' || matrx[2][0] == 'x' || '0')
+		return 'x';
+}*/
